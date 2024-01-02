cd123bc18cd3b76b84aa8460f5521b00f2d1615a
lauichan <te06230623@gmail.com>
2024-01-02 20:00:40 +0900

    test

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const options = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkODI4OWFhMmJlYzAyYmQ3OGFmNjdmODIzNDNkMDhjOCIsInN1YiI6IjY1OTNiNjdkZWJiOTlkNWUxN2EwMTQ1NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.U9hqR5oF0oqXrbFgqXlC2yY1zj4JGJshGMzZfoGuuLw'
-    }
+const fetchData = () => {
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkODI4OWFhMmJlYzAyYmQ3OGFmNjdmODIzNDNkMDhjOCIsInN1YiI6IjY1OTNiNjdkZWJiOTlkNWUxN2EwMTQ1NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.U9hqR5oF0oqXrbFgqXlC2yY1zj4JGJshGMzZfoGuuLw'
+        }
+    };
+
+    fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+        .then(response => response.json())
+        .then(response => createCard(response))
+        .catch(err => console.error(err));
 };
-
-fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-    .then(response => response.json())
-    .then(response => createCard(response))
-    .catch(err => console.error(err));
-
-document.getElementById("searchInput").focus();
 
 function createCard(response) {
     let movies = response.results;
     movies.forEach(movie => {
         document.getElementById("movies").innerHTML += `
-            <li>
+            <li onclick="alert('영화id: ${movie.id}')">
                 <img class="poster" src="http://image.tmdb.org/t/p/w300${movie.poster_path}"/>
                 <h2 class="title">${movie.title}</h2>
                 <p class="overview">${movie.overview}</p>
@@ -34,3 +34,7 @@ function searchMovie(event) {
         title.includes(keyword) ? li.style.display = "block" : li.style.display = "none";
     });
 }
+
+fetchData();
+
+document.getElementById("searchInput").focus();
