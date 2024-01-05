97780e6dd7731a316fe2acc118d31fc180748d58
lauichan <te06230623@gmail.com>
2024-01-05 16:57:47 +0900

    add korean genre list

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const genreList = [
-    { id: 28, name: "Action" },
-    { id: 12, name: "Adventure" },
-    { id: 16, name: "Animation" },
-    { id: 35, name: "Comedy" },
-    { id: 80, name: "Crime" },
-    { id: 99, name: "Documentary" },
-    { id: 18, name: "Drama" },
-    { id: 10751, name: "Family" },
-    { id: 14, name: "Fantasy" },
-    { id: 36, name: "History" },
-    { id: 27, name: "Horror" },
-    { id: 10402, name: "Music" },
-    { id: 9648, name: "Mystery" },
-    { id: 10749, name: "Romance" },
-    { id: 878, name: "Science Fiction" },
-    { id: 10770, name: "TV Movie" },
-    { id: 53, name: "Thriller" },
-    { id: 10752, name: "War" },
-    { id: 37, name: "Western" }
+    { id: 28, en: "action", ko: "액션" },
+    { id: 12, en: "adventure", ko: "모험" },
+    { id: 16, en: "animation", ko: "애니메이션" },
+    { id: 35, en: "comedy", ko: "코미디" },
+    { id: 80, en: "crime", ko: "범죄" },
+    { id: 99, en: "documentary", ko: "다큐멘터리" },
+    { id: 18, en: "drama", ko: "드라마" },
+    { id: 10751, en: "family", ko: "가족" },
+    { id: 14, en: "fantasy", ko: "판타지" },
+    { id: 36, en: "history", ko: "역사" },
+    { id: 27, en: "horror", ko: "공포" },
+    { id: 10402, en: "music", ko: "음악" },
+    { id: 9648, en: "mystery", ko: "미스터리" },
+    { id: 10749, en: "romance", ko: "로맨스" },
+    { id: 878, en: "science Fiction", ko: "SF" },
+    { id: 10770, en: "tv_movie", ko: "TV 영화" },
+    { id: 53, en: "thriller", ko: "스릴러" },
+    { id: 10752, en: "war", ko: "전쟁" },
+    { id: 37, en: "western", ko: "서부" }
 ];
 
 const fetchData = async () => {
@@ -39,40 +39,41 @@ const fetchData = async () => {
 };
 
 function createCard(response) {
-    let movies = response.results;
+    const movies = response.results;
     movies.forEach(movie => {
-        document.getElementById("movies").innerHTML += `
-            <div id="${movie.id}" class="card" onclick="alert('영화id: ${movie.id}')">
-                <img class="poster" src="https://image.tmdb.org/t/p/w300${movie.poster_path}" width=300 height=450/>
-                <h2 class="title">${movie.title}</h2>
-                <p class="overview">${movie.overview}</p>
-                <ul class="genre"></ul>
-                <p class="vote"><span class="star">★</span>${(movie.vote_average * 10).toFixed(2)}</p>
-            </div>`;
+        document.getElementById("movies").innerHTML += cardHTML(movie);
         createGenreList(movie.id, movie.genre_ids);
     });
 }
 
+function cardHTML(movie) {
+    return `<div id="${movie.id}" class="card" onclick="alert('영화id: ${movie.id}')">
+                <img class="poster" src="https://image.tmdb.org/t/p/w300${movie.poster_path}" title="${movie.id}" width=300 height=450/>
+                <h2 class="title">${movie.title}</h2>
+                <p class="overview">${movie.overview}</p>
+                <ul class="genre"></ul>
+                <p class="vote"><span class="star">★</span>${(movie.vote_average * 10).toFixed(2)}</p>
+            </div>`
+}
+
 function createGenreList(ele_id, genre_ids) {
-    let genreName = genre_ids.map(id => {
-        let obj = genreList.find(genre => genre.id === id);
-        return obj.name;
-    });
+    const genreName = genre_ids.map(id => obj = genreList.find(genre => genre.id === id));
     genreName.forEach(genre => {
-        document.getElementById(`${ele_id}`).querySelector(".genre").innerHTML += `<li class="${genre.toLowerCase()}">${genre}</li>`
+        document.getElementById(`${ele_id}`).querySelector(".genre").innerHTML += `<li class="${genre.en}">${genre.ko}</li>`
     })
 }
 
 function searchMovie(event) {
     event.preventDefault();
     const keyword = document.querySelector("#searchInput").value.toLowerCase();
-    [...document.querySelectorAll("#movies > .card")].forEach((card) => {
+    const cards = [...document.querySelectorAll("#movies > .card")];
+    cards.forEach((card) => {
         card.classList.toggle("hide", !searchBy(keyword, card));
     });
 }
 
 function searchBy(keyword, card) {
-    const text = card.querySelector("h2").textContent.toLowerCase() + card.querySelector(".genre").textContent.toLowerCase();
+    const text = card.textContent.toLowerCase();
     return text.includes(keyword);
 }
 
