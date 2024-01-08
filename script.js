27d86475c847d67533f349efb46b24e30d3c1dec
lauichan <te06230623@gmail.com>
2024-01-08 14:18:47 +0900

    prettier 설정

--- script.js
+++ script.js
@@ -24,10 +24,11 @@ let currentPage = 1;
 
 const fetchData = async (page) => {
     const options = {
-        method: 'GET',
+        method: "GET",
         headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkODI4OWFhMmJlYzAyYmQ3OGFmNjdmODIzNDNkMDhjOCIsInN1YiI6IjY1OTNiNjdkZWJiOTlkNWUxN2EwMTQ1NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.U9hqR5oF0oqXrbFgqXlC2yY1zj4JGJshGMzZfoGuuLw'
+            accept: "application/json",
+            Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkODI4OWFhMmJlYzAyYmQ3OGFmNjdmODIzNDNkMDhjOCIsInN1YiI6IjY1OTNiNjdkZWJiOTlkNWUxN2EwMTQ1NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.U9hqR5oF0oqXrbFgqXlC2yY1zj4JGJshGMzZfoGuuLw"
         }
     };
 
@@ -44,7 +45,7 @@ const fetchData = async (page) => {
 
 function createCard(response) {
     const movies = response.results;
-    movies.forEach(movie => {
+    movies.forEach((movie) => {
         document.getElementById("movies").innerHTML += cardHTML(movie);
         createGenreList(movie.id, movie.genre_ids);
     });
@@ -57,14 +58,16 @@ function cardHTML(movie) {
                 <p class="overview">${movie.overview}</p>
                 <ul class="genre"></ul>
                 <p class="vote"><span class="star">★</span>${(movie.vote_average * 10).toFixed(1)}%</p>
-            </div>`
+            </div>`;
 }
 
 function createGenreList(ele_id, genre_ids) {
-    const genreName = genre_ids.map(id => obj = genreList.find(genre => genre.id === id));
-    genreName.forEach(genre => {
-        document.getElementById(`${ele_id}`).querySelector(".genre").innerHTML += `<li class="${genre.en}">${genre.ko}</li>`
-    })
+    const genreName = genre_ids.map((id) => (obj = genreList.find((genre) => genre.id === id)));
+    genreName.forEach((genre) => {
+        document
+            .getElementById(`${ele_id}`)
+            .querySelector(".genre").innerHTML += `<li class="${genre.en}">${genre.ko}</li>`;
+    });
 }
 
 function searchMovie(event) {
@@ -84,7 +87,7 @@ function searchBy(keyword, card) {
 function newPage() {
     currentPage++;
     fetchData(currentPage);
-    document.getElementById("searchInput").setAttribute("placeholder", `검색어를 입력해주세요.`)
+    document.getElementById("searchInput").setAttribute("placeholder", `검색어를 입력해주세요.`);
 }
 
 fetchData(currentPage);
