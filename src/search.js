aac857b264bfb9ee57b68d5a2c7a1d26d82d5fcb
lauichan <te06230623@gmail.com>
2024-01-09 23:44:56 +0900

    커밋하면서 잘못 올린거 수정

--- src/search.js
+++ src/search.js
@@ -1,10 +1,12 @@
 export function searchMovie(event) {
     event.preventDefault();
     const keyword = document.querySelector("#searchInput").value.toLowerCase();
-    const cards = document.querySelector("#movies");
+    const cards = document.querySelectorAll("#movies > div");
 
-    let result = [...cards].some((card) => {
-        const text = card.textContent.toLowerCase().includes(keyword);
+    let result = [...cards].filter((card) => {
+        const text = (card.querySelector(".title").textContent + card.querySelector("ul").textContent)
+            .toLowerCase()
+            .includes(keyword);
         card.classList.toggle("hide", !text);
         return text;
     });
